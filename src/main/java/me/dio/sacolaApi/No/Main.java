54893859c2638344f2dac3f15357545329bc7be9
Daniela2319 <frankvt04@gmail.com>
2022-10-03 09:58:27 -0300

    conceito de No

---
+++ src/main/java/me/dio/sacolaApi/No/Main.java
@@ -0,0 +1,32 @@
+package me.dio.sacolaApi.No;
+
+import me.dio.sacolaApi.No.No;
+
+public class Main {
+
+    // Na hora de declara são tipo Strings
+
+    public static void main(String[] args) {
+        No<String> no1 = new No<>("Conteudo no1");
+
+        No<String> no2 = new No<>("Conteudo no2");
+        no1.setProximoNo(no2);
+
+        No<String> no3 = new No<>("Conteudo no3");
+        no2.setProximoNo(no3);
+
+        No<String> no4 = new No<>("Conteudo no4");
+        no3.setProximoNo(no4);
+
+        // no1-> no2 -> no3 -> no4 -> null termina o encadeamento
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+
+
+
+    }
+}
